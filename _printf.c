5bf1486dc5e8ae1ee8c2570ad42801434dcbb8a1
mjcry <mj.masilo.jack@gmail.com>
2022-10-17 14:00:06 -0700

    git

--- _printf.c
+++ _printf.c
@@ -1,44 +1,86 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
- *_printf - will recreate the printf function in C (specifiers limited)
- *@format: a character string composed of 0 or more directives
- *Return: the number of characters printed
+ *check_format - checks if there is a valid format specifier
+ *@format: possible valid format specifier
+ *Return: pointer to valid function or NULL
  */
+
+int (*check_format(const char *format))(va_list)
+{
+	int i = 0;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"b", print_b},
+		{"u", print_u},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+
+	for (i=0; p[i].type != NULL; i++)
+	{
+		if (*(p[i].type) == *format)
+			break;
+	}
+	return (p[i].function);
+}
+
+/**
+ *_printf - function for format printing
+ *@format: list of arguments to printing
+ *Return: Number of characters to printing
+ */
+
 int _printf(const char *format, ...)
 {
-	int (*type)(va_list);
-	va_list list;
-	int i;
-	int count = 0;
+	va_list name;
+	int (*function)(va_list);
+	unsigned int i = 0, counter = 0;
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(list, format);
-	for (i = 0; format[i] != '\0'; i++)
+	va_start(name, format);
+	while (format && format[i])
 	{
-		if (format[i] != '%' && format[i])
+		if (format[i] != '%')
 		{
 			_putchar(format[i]);
-			count++;
-		}
-		if (format[i] == '%')
-		{
-			type = get_conversion(format[i + 1]);
-				if (type != NULL)
-					count = count + (type(list));
-				else
-				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					count++;
-				}
+			counter++;
 			i++;
+			continue;
 		}
-		if (!format[i])
-			return (count);
+		else
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				counter++;
+				i += 2;
+				continue;
+			}
+			else
+			{
+				function = check_format(&format[i + 1]);
+				if (function == NULL)
+					return (-1);
+				i += 2;
+				counter += function(name);
+				continue;
+			}
+		}
 	}
-	va_end(list);
-	return (count);
+	va_end(name);
+	return (counter);
 }
